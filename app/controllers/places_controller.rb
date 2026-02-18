06d225ab3b007c6b314f26bd199d869b3af6a718
Matt <matthew.martin@kellogg.northwestern.edu>
2026-02-18 16:09:15 +0000

    Built places controller

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,2 +1,26 @@
 class PlacesController < ApplicationController
+  def index
+    @places = place.all
+    #render places/index view
+  end
+
+
+  def new
+    #render places/new view with new place form
+  end
+
+
+  def create
+    # start with a new Place
+    @place = Place.new
+
+    #assign user-entered form data to Place's columns
+    @place["name"] = params["name"]
+
+    # save Place row
+    @place.save
+
+    #redirect user
+    redirect_to "/"
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   get("/entries", {:controller => "entries", :action => "index"})
-  get("/places", {:controller => "places", :action => "index"})
-  # Defines the root path route ("/")
-  get("/", { :controller => "articles", :action => "index" })
+   # Defines the root path route ("/")
+  get("/", {:controller => "places", :action => "index"})
 end
