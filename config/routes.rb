2992aaef255eb870e4d3f098dab5ae918a638308
Matt <matthew.martin@kellogg.northwestern.edu>
2026-02-18 19:15:46 +0000

    Added a general routing

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   get("/entries", {:controller => "entries", :action => "index"})
-  
-   # Defines the root path route ("/")
   get("/places", {:controller => "places", :action => "index"})
+   # Defines the root path route ("/")
+  get("/", {:controller => "places", :action => "index"})
   
   resources :places
   resources :entries
